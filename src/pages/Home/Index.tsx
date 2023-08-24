e454de596d7ba75c525248e7f29d0c13827bbef5
Fanderson Kaecio Valencio <fanderson.kaecio@gmail.com>
2023-08-24 15:36:32 -0300

    feature(FDB-7):Create the main page

--- src/pages/Home/Index.tsx
+++ src/pages/Home/Index.tsx
@@ -3,7 +3,15 @@ import * as Style from './Styles';
 export const Home = () => {
   return (
     <Style.Wrapper>
-      <Style.Title>Seja um Fan Digital</Style.Title>
+      <Style.Container>
+        <Style.Title>Seja um fan digital</Style.Title>
+        <Style.Description>
+          Tenha uma experiencian incrivel com um banco que é a sua cara
+        </Style.Description>
+        <Style.SubTitle>Abra a sua conta!</Style.SubTitle>
+        <Style.Button_OpenAccount>Comece por aqui!</Style.Button_OpenAccount>
+      </Style.Container>
+      <Style.Container_middle>parte2</Style.Container_middle>
     </Style.Wrapper>
   );
 };
--- src/pages/Home/Styles.ts
+++ src/pages/Home/Styles.ts
@@ -1,16 +1,40 @@
 import styled from 'styled-components';
 
 export const Wrapper = styled.main`
-  background-color: #3d3d3d;
-  color: #fff;
   height: 100vh;
   width: 100vw;
+  background-attachment: scroll;
 `;
 
-export const Img = styled.img``;
+export const Img = styled.img`
+  width: 100vw;
+  height: 100vh;
+  background-size: cover;
+  background-position: center;
+  border: none;
+`;
 
+export const Container = styled.section`
+  background-image: url('/public/img/back-ground1.jpeg');
+  height: 100%;
+  width: 100%;
+  background-color: #3d3d3d;
+`;
+
+export const Container_middle = styled.section`
+  height: 100%;
+  width: 100%;
+  background-color: #3d3d3d;
+`;
 export const Title = styled.h1``;
 
 export const SubTitle = styled.h2``;
 
 export const Description = styled.h3``;
+
+export const Button_OpenAccount = styled.button`
+  width: 130px;
+  height: 30px;
+  background-color: #00aeef;
+  border-radius: 7px;
+`;
